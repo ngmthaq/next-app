0ea61450fda5d496cb902adb43684e10398a41f3
thangnm <thangnm@foobla.com>
2024-04-15 15:25:27 +0700

    Init Firebase & Firebase Admin

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,13 +1,15 @@
 // Import Normal Data
 import * as Constants from "./constants";
 import * as Paths from "./paths";
+import * as FirebaseClient from "./firebaseClient";
+import * as FirebaseAdmin from "./firebaseAdmin";
 import EventBus from "./eventBus";
 
 // Import Types
 import type { TODO, Nullable } from "./types";
 
 // Export Normal Data
-export { Constants, Paths, EventBus };
+export { Constants, Paths, EventBus, FirebaseClient, FirebaseAdmin };
 
 // Export Types
 export type { TODO, Nullable };
